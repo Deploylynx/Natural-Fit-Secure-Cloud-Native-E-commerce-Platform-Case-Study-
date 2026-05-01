ea969ebfc31eb9f257e1507d06bace69cbe7de03
Hina-Atif <hinaatif355@gmail.com>
2026-05-01 12:45:02 +0500

    add auto deploy to EC2

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function add(a, b) {
-  return a + b;
-}
+const express = require("express");
+const app = express();
 
-console.log(add(10, 20));
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("NaturalFit Herbal Products 🌿 — Empowering healthier lives through nature-driven solutions. Experience purity, quality, and wellness you can trust.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
