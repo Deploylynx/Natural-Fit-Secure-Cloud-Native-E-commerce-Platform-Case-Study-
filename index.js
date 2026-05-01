ac8be406bdf3a97fd5b1f37dffafc7509e90d600
Hina-Atif <hinaatif355@gmail.com>
2026-05-01 12:51:08 +0500

    restore server code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("NaturalFit Herbal Products 🌿 — Empowering healthier lives through nature-driven solutions. Experience purity, quality, and wellness you can trust.");
+  res.send("Welcome to NaturalFit Herbal Products 🌿 — Delivering pure, natural, and effective wellness solutions for a healthier lifestyle. Trusted quality, inspired by nature.");
 });
 
 app.listen(PORT, () => {
